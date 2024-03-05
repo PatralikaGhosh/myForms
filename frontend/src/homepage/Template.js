e700fbbad376d3fe6e4310daec0b86b12c713f55
Patralika Ghosh <ghoshpatra2002@gmail.com>
2024-03-05 11:44:27 -0600

    Added header and tabs

--- frontend/src/homepage/Template.js
+++ frontend/src/homepage/Template.js
@@ -4,8 +4,15 @@ import {IconButton} from '@material-ui/core';
 import './Template.css';
 import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
 import blank from '../images/blank_form.png';
+import uuid from 'react-uuid';
+import { useNavigate } from 'react-router-dom';
 
 function Template() {
+    const navigate = useNavigate();
+    const createForm = () => {
+        const id = uuid();
+        navigate(`/form/${id}`);
+    }
   return (
     <div className="template_section">
         <div className="template_top">
@@ -23,7 +30,7 @@ function Template() {
             </div>
         </div>
         <div className="template_body">
-            <div className="template_card">
+            <div className="template_card" onClick={createForm}>
                 <img src={blank} alt="no image" className="card_image"/>
                 <p className="card_title">Blank</p>
             </div>
